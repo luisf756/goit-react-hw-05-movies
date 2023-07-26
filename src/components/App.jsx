b5b9d3593f6f756c189c97e674870480d156ec80
luisf756 <luchof756@gmail.com>
2023-07-26 00:59:12 -0500

    add routes and pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,10 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "../pages/Home";
+import Movies from "../pages/Movies";
+import MovieDetails from "../pages/MovieDetails";
+// import NotFound from "path/to/pages/NotFound";
+// import ProductDetails from "path/to/pages/ProductDetails";
+
 export const App = () => {
   return (
     <div
@@ -10,7 +17,25 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/movies">Movies</Link>
+        {/* <Link to="/products">Products</Link> */}
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />}></Route>
+        <Route path="/movies/:productId" element={<MovieDetails />} />
+          {/* <Route path="mission" element={<Mission />} />
+          <Route path="team" element={<Team />} />
+          <Route path="reviews" element={<Reviews />} /> */}
+        
+        {/* <Route path="/products" element={<Products />} />
+        <Route path="/products/:productId" element={<ProductDetails />} />
+        <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </div>
     </div>
   );
 };
---
+++ src/pages/Movies.jsx
@@ -0,0 +1,23 @@
+// import { Link, Outlet } from "react-router-dom";
+
+ const About = () => {
+  return (
+    <div>
+      <h2>Find movie</h2>
+      <ul>
+        <li>
+          {/* <Link to="mission">Read about our mission</Link> */}
+        </li>
+        <li>
+          {/* <Link to="team">Get to know the team</Link> */}
+        </li>
+        <li>
+          {/* <Link to="reviews">Go through the reviews</Link> */}
+        </li>
+      </ul>
+      {/* <Outlet /> */}
+    </div>
+  );
+};
+
+export default About
